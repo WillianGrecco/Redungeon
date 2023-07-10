480ac063b73a687ba1c24b09dc2e0390b3fc0afc
WillianGrecco <124214097+WillianGrecco@users.noreply.github.com>
2023-07-10 13:04:00 -0300

    Add files via upload

--- Jogo Redungeon/js/Game.js
+++ Jogo Redungeon/js/Game.js
@@ -17,19 +17,23 @@ class Game {
     };
                
     
-
-    start(){
-    // player = new player();
-    form = new forms();
-    }
-        
     start() {
+        player = new Player();
+        
+        forms = new forms();
+        forms.display();
 
+        cavaleiro1.addImage("cavaleiro1", atras.png);
+        cavaleiro1.scale = 0.07;
 
+        cavaleiro2.addImage("cavaleiro2", frente.png);
+        cavaleiro2.scale = 0.07;
+        
+        cavaleiro3.addImage("cavaleiro3", esquerda.png);
+        cavaleiro3.scale = 0.07;
 
-
-
-
+        cavaleiro4.addImage("cavaleiro2", direita.png);
+        cavaleiro4.scale = 0.07;
     }
 
 
